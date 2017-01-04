1435ff4533a0d346dc3e9f22c83a26129bcf59ef
ewa <ewa.listy@gmail.com>
2017-01-04 13:37:10 +0100

    added sensor status and type

--- postgresql/SmartHome_create.sql
+++ postgresql/SmartHome_create.sql
@@ -1,6 +1,12 @@
 -- Last modification date: 2016-12-31 14:51:16.07
 
 -- tables
+
+DROP TABLE Alerts ;
+DROP TABLE Measurments;
+DROP TABLE Sensors;
+
+
 -- Table: Alerts
 CREATE TABLE Alerts (
     sensor varchar(20)  NOT NULL,
@@ -18,26 +24,29 @@ CREATE TABLE Measurments (
 -- Table: Sensors
 CREATE TABLE Sensors (
     mac varchar(20)  NOT NULL,
-    type int  NOT NULL,
+    type int  NOT NULL, --1 - temp , 2- motion, 3- heater
     description varchar(100)  NOT NULL,
-    CONSTRAINT Sensors_pk PRIMARY KEY (mac)
+    status int NOT NULL,
+    CONSTRAINT Sensors_pk PRIMARY KEY (mac, type)
 );
 
--- foreign keys
--- Reference: Alerts_Sensors (table: Alerts)
-ALTER TABLE Alerts ADD CONSTRAINT Alerts_Sensors
-    FOREIGN KEY (sensor)
-    REFERENCES Sensors (mac)
-    NOT DEFERRABLE
-    INITIALLY IMMEDIATE
-;
+INSERT INTO Sensors (mac, type, description, status) VALUES (
+  '00126FC21D7C', 1, 'drzwi wejsciowe', 1
+);
 
--- Reference: Measurments_Sensors (table: Measurments)
-ALTER TABLE Measurments ADD CONSTRAINT Measurments_Sensors
-    FOREIGN KEY (sensor)
-    REFERENCES Sensors (mac)
-    NOT DEFERRABLE
-    INITIALLY IMMEDIATE
-;
+INSERT INTO Sensors (mac, type, description, status) VALUES (
+  '00126FC21D7C', 2, 'drzwi wejsciowe', 1
+);
+
+INSERT INTO Sensors (mac, type, description, status) VALUES (
+  '00126FC21D7C', 1, 'drzwi wejsciowe', 1
+);
+
+INSERT INTO Sensors (mac, type, description, status) VALUES (
+  '00126FC21D7C', 1, 'drzwi wejsciowe', 1
+);
+
+commit;
+
 
 -- End of file.
