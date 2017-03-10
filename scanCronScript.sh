221a935c05ab73cb83ee070816c41f56f1fa4722
ewa <ewa.listy@gmail.com>
2017-03-10 14:56:41 +0100

    cron task script

--- scanCronScript.sh
+++ scanCronScript.sh
@@ -3,7 +3,8 @@
 case "$(pidof -x readTempAndMotionDetector.sh | wc -w)" in
 
 0)  echo "Restarting Scanning Script:     $(date)" >> /var/log/scanner.txt
-    cd /home/pi/dev/RPiScripts & sudo ./readTempAndMotionDetector.sh
+    cd /home/pi/dev/RPiScripts
+    sudo ./readTempAndMotionDetector.sh &
     ;;
 2)  # all ok
     echo "Script is running correctly:     $(date)" >> /var/log/scanner.txt
